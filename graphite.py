34820b0968a7827e8c5e6a4df3ef0f609542347a
kryczko <kevinryczko@me.com>
2013-05-12 12:20:24 -0400

    finally added PBC's into code

--- graphite.py
+++ graphite.py
@@ -2,11 +2,15 @@ from ase.lattice.hexagonal import *
 import ase.io as io
 from ase import Atoms, Atom
 
-index1=6
-index2=7
+index1=20
+index2=18
 mya = 2.46
 myc = 6.70 
 
+stacks = 1 
+
 gra = Graphite(symbol = 'C',latticeconstant={'a':mya,'c':myc},
-               size=(index1,index2,4))
-io.write('graphite_8_layers.xyz', gra, format='xyz')
+               size=(index1,index2,stacks))
+io.write('test.xyz', gra, format='xyz')
+
+print "The lattice vector is {", index1*mya, 1.42*index2/2 + 2.84*index2/2 , stacks*6.70, "}"
